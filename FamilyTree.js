4c43adf2b16edcfebb48554f768e5a7d28bb3a8d
Mark Wong <markwongsk@gmail.com>
2013-05-03 14:39:00 -0400

    Fixed the mofarrell's bug.
    Reason: modified font after measuretext

--- FamilyTree.js
+++ FamilyTree.js
@@ -2,6 +2,8 @@
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+
 var MINIMAL_SPACING = 35;
 var VERTICAL_SPACING = 50;
 var SEMESTER_NOW = 8;
@@ -13,24 +15,24 @@ var SEMESTER_NOW = 8;
 // pos - the position this CA's name will be drawn
 //       (anchored at south)
 // semester - the semester the CA first CAed
-// active - whether this CA is being displayed 
-function CA(andrewid, children, pos, semester, active) {
+// activeCount - the number of active references to this CA
+function CA(andrewid, children, pos, semester, activeCount) {
   this.andrewid = andrewid;
   this.children = children;
   this.pos = pos;
   this.semester = semester;
   semesterToCA[semester].push(this);
-  this.active = (active === undefined ? false : active);
-
+  this.activeCount = (activeCount === undefined ? 0 : activeCount);
+  this.clicked = false;
 }
 
 // gets the bounding box for where this CA's name is being drawn
 CA.prototype.getBBox = function() {
-  var measure = ctx.measureText(andrewid);
-  console.log("Getting bbox of: " + ca);
-  var cx = ca.pos[0];
-  var cy = ca.pos[1];
-  return new bBox(cx-measure.width/2, cy-2*VERTICAL_SPACING/3,
+  var measure = ctx.measureText(this.andrewid);
+  console.log("Getting bbox of: " + this.andrewid);
+  var cx = this.pos[0];
+  var cy = this.pos[1];
+  return new BBox(cx-measure.width/2, cy-2*VERTICAL_SPACING/3,
                   cx+measure.width/2, cy)
 }
 
@@ -40,7 +42,7 @@ CA.prototype.addChild = function(child) {
 }
 
 // BBox class
-function bBox(left, top, right, bottom) {
+function BBox(left, top, right, bottom) {
   this.left = left;
   this.top = top;
   this.right = right;
@@ -49,6 +51,7 @@ function bBox(left, top, right, bottom) {
 
 function drawFamilyTree() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log("In draw family tree!");
     var activeCAs = repositionCAs(semesterToCA);
     for (var s = 0; s < SEMESTER_NOW; s++) {
         for (var i = 0; i < activeCAs[s].length; i ++) {
@@ -66,7 +69,7 @@ function drawFamilyTree() {
 function drawConnections(ca) {
     console.log(ca.children.length);
     for (var i = 0; i < ca.children.length; i ++) {
-        if (ca.children[i].active) {
+        if (ca.children[i].activeCount > 0) {
             drawSegment(ca.pos[0], ca.pos[1],
                         ca.children[i].pos[0],
                         ca.children[i].pos[1]);
@@ -86,13 +89,13 @@ function drawSegment(x1, y1, x2, y2) {
 function drawNode(x, y, value) {
     var w = ctx.measureText(value);
     console.log("Drawing: " + value + " at (" + x + "," + y + ")");
+    console.log(w);
     ctx.rect(x-w.width/2,y-2*VERTICAL_SPACING/3, w.width,2*VERTICAL_SPACING/3);
     ctx.fillStyle = "blue";
     ctx.fill();
 
     ctx.beginPath();
     ctx.fillStyle = "black";
-    ctx.font = "30px Arial";
     ctx.textAlign = "center";
     ctx.fillText(value, x, y);
 }
@@ -102,7 +105,7 @@ function repositionCAs(semesterToCAs) {
     for (var s = 0; s < SEMESTER_NOW; s++) {
         activeCAs[s] = [];
         for (var i = 0; i < semesterToCAs[s].length; i ++) {
-            if (semesterToCAs[s][i].active) {
+            if (semesterToCAs[s][i].activeCount > 0) {
                 activeCAs[s].push(semesterToCAs[s][i]);
             }
         }
@@ -130,23 +133,13 @@ for (var i = 0; i < SEMESTER_NOW; i++) {
   semesterToCA[i] = [];
 }
 
-function getbBox(ca) {
-  var text = ca.andrewid;
-  var measure = ctx.measureText(text);
-  console.log("Getting bbox of: " + ca);
-  var cx = ca.pos[0];
-  var cy = ca.pos[1];
-  return new bBox(cx-measure.width/2, cy-2*VERTICAL_SPACING/3,
-              cx+measure.width/2, cy)
-}
-
 function getCAAtCoord(x,y) {
   console.log (semesterToCA);
   for (var semester in semesterToCA) {
     for (var i = 0; i < semesterToCA[semester].length; i++) {
       var ca = semesterToCA[semester][i];
       console.log(ca);
-      var bbox = getbBox(ca);
+      var bbox = ca.getBBox();
       console.log(bbox);
       if (x >= bbox.left && x <= bbox.right &&
           y >= bbox.top && y <= bbox.bottom) {
@@ -166,9 +159,16 @@ function onMouseDown(event) {
 
   if (ca !== null) {
     console.log("You clicked: " + ca.andrewid);
-    for (var i = 0; i < ca.children.length; i++) {
-      ca.children[i].active = true;
+    if (ca.clicked) {
+      for (var i = 0; i < ca.children.length; i++) {
+        ca.children[i].activeCount--;
+      }
+    } else {
+      for (var i = 0; i < ca.children.length; i++) {
+        ca.children[i].activeCount++;
+      }
     }
+    ca.clicked = !ca.clicked;
   }
 
   drawFamilyTree();
@@ -185,5 +185,6 @@ asdf = new CA("asdf", [], [0,0], 1, false);
 yeah = new CA("yeah", [], [0,0], 1, false);
 kelly = new CA("krivers", [asdf, yeah], [0,0], 0, true);
 
+console.log("WHAT THE FUCK");
 drawFamilyTree();
 
