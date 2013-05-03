605b8f7c154d931b1f160cf4e02d446dc9be02d2
mofarrell <micpof@gmail.com>
2013-05-03 12:04:20 -0400

    Added code for repositioning CAs and drawing them..

--- FamilyTree.js
+++ FamilyTree.js
@@ -2,16 +2,18 @@
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
-
+var MINIMAL_SPACING = 35;
+var VERTICAL_SPACING = 50;
 
 function drawFamilyTree() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawNode(canvas.width/2,50, "CA 1");
-    drawSegment(canvas.width/2, 50, canvas.width/3,75);
-    drawNode(canvas.width/3,100, "CA 1");
-    drawSegment(canvas.width/2, 50, 2*canvas.width/3,75);
-    drawNode(2*canvas.width/3,100, "CA 1");
+    activeCAs = repositionCAs(semesterToCAs);
+    for (s = 0; s < SEMESTER_NOW; s++) {
+        for (i = 0; i < activeCAs[s].length; i ++) {
+            drawNode(activeCAs[s][i].pos[0], activeCAs[s][i].pos[1],
+                    activeCAs[s][i].name);
+        }
+    }
 }
 
 function drawSegment(x1, y1, x2, y2) {
@@ -27,6 +29,30 @@ function drawNode(x, y, value) {
     ctx.textAlign = "center";
     ctx.fillText(value, x, y);
 }
+
+function repositionCAs(semesterToCAs) {
+    activeCAs = {};
+    for (s = 0; s < SEMESTER_NOW; s++) {
+        activeCAs[s] = [];
+        j = 0
+        for (i = 0; i < semesterToCAs[s].length; i ++) {
+            if (semesterToCAs[s][i].active) {
+                activeCAs[s][j].push(semesterToCAs[s][i]);
+                j ++;
+            }
+        }
+        spacing = canvas.width/(activeCAs[s].length+1);
+        if (spacing < MINIMAL_SPACING) {
+            resize(canvas.width*2, canvas.height*2);
+            return;
+        }
+        for (i = 0; i < activeCAs[s].length; i++) {
+            activeCAs[s][i].pos = [spacing*(i+1), (s+1)*VERTICAL_SPACING];
+        }
+    }
+    return activeCAs;
+}
+    
 
 function resize(width, height) {
     canvas.width = width;
