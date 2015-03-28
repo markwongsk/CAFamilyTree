6067a2f7889ac7f8800af3f46b7a2771bd52ea30
Mark Wong Siang Kai <mwong@palantir.com>
2015-03-28 14:27:16 -0700

    Restarted from scratch
    -- use dagre-d3 as renderer and layout engine
    -- graph appears, and doesn't look as janky as d3's force directed graph

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,222 @@
+/**
+ * The build directory's layout is::
+ *
+ * build/
+ * ├── bower_components
+ * ├── css
+ * ├── images
+ * ├── index.html
+ * ├── karma.conf.js
+ * ├── src
+ * └── test
+ */
+
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-typescript');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('typescript-tpm');
-    
+    // Runs `load-grunt-task` to automatically load NPM Tasks that start with `grunt-`
+    // For more information, see the docs: http://firstandthird.github.io/load-grunt-config/
+    require("load-grunt-config")(grunt);
+
+    // =======
+    // Helpers
+    // =======
+    var sourceWildcards = [
+        "**/*.coffee",
+        "**/*.ico",
+        "**/*.ts",
+        "**/*.js",
+        "**/*.css",
+        "**/*.less",
+        "**/*.sass",
+        "**/*.html",
+        "**/*.svg"
+    ];
+
+    function sourceWildcardsIn(prefix) {
+        return sourceWildcards.map( function (wildcard) {
+            return (prefix + wildcard);
+        });
+    }
+
+    function includeTypings(files) {
+        return files.concat([
+            "bower_components/**/*.d.ts",
+            "!bower_components/**/{bower_components,typings}/**",
+            "typings/**/*.d.ts"
+        ]);
+    }
+
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        clean: {
-            build: {
-                src: ['js/**/*.js', 'js/**/*.js.map']
-            }
-        },
 
-        'tpm-install': {
-            options: {
-                dev: false
-            },
-            all: {
-                src: 'package.json',
-                dest: 'lib/typings/'
-            }
-        },
-
-        typescript: {
-            base: {
-                src: ['src/**/*.ts', 'src/**/*.d.ts'],
-                dest: 'js',
+        // =================
+        // grunt-notify-task
+        // =================
+        notify: {
+            watch: {
                 options: {
-                    module: 'amd',
-                    target: 'es5'
+                    message: "grunt watch complete"
                 }
             }
         },
 
+
+        // ===================
+        // grunt-contrib-clean
+        // ===================
+        clean: {
+            build: ["build/"]
+        },
+
+
+        // ==================
+        // grunt-contrib-copy
+        // ==================
+        copy: {
+            src: {
+                files: [
+                    {
+                        expand: true,
+                        src: "bower_components/**",
+                        dest: "build/"
+                    },
+                    {
+                        expand: true,
+                        src: "images/**",
+                        dest: "build/"
+                    },
+                    {
+                        expand: true,
+                        cwd: "src/",
+                        src: "index.html",
+                        dest: "build/"
+                    },
+                    {
+                        expand: true,
+                        src: "assets/**",
+                        dest: "build/"
+                    },
+                    {
+                        expand: true,
+                        src: "src/**/*.html",
+                        dest: "build/"
+                    },
+                    {
+                        expand: true,
+                        src: "src/**/*.ts",
+                        dest: "build/"
+                    },
+                ]
+            },
+        },
+
+        // ==================
+        // grunt-contrib-less
+        // ==================
+        less: {
+            src: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "src/",
+                        src: ["**/*.less"],
+                        dest: "build",
+                        ext: ".css"
+                    }
+                ]
+            },
+        },
+
+        // ========
+        // grunt-ts
+        // ========
+        ts: {
+            options: {
+                failOnTypeErrors: true,
+                fast: "never",
+                noImplicitAny: true,
+                removeComments: false,
+                target: "es5",
+                sourceRoot: "/"
+            },
+
+            src: {
+                options: {
+                    declaration: true
+                },
+                src: includeTypings(["src/**/*.ts"]),
+                outDir: "build/src"
+            }
+        },
+
+
+        // ===================
+        // grunt-contrib-watch
+        // ===================
         watch: {
-            files: 'src/**/*.ts',
-            tasks: 'typescript'
+            options: {
+                // to use, make sure to add the following snippet before the closing </body> tag:
+                //
+                //     <script src="//localhost:35729/livereload.js"></script>
+                //
+                // or use the Chrome extension:
+                //
+                //     https://github.com/gruntjs/grunt-contrib-watch#using-live-reload-with-the-browser-extension
+                //
+                // for more information:
+                //
+                //     https://github.com/gruntjs/grunt-contrib-watch#enabling-live-reload-in-your-html
+                livereload: true
+            },
+
+            bower: {
+                files: "bower.json",
+                tasks: ["copy", "wiredep"]
+            },
+
+            copy: {
+                files: sourceWildcardsIn("src/"),
+                tasks: ["copy", "wiredep"]
+            },
+
+            less: {
+                files: "src/**/*.less",
+                tasks: "less:src"
+            },
+
+            ts: {
+                files: "src/**/*.ts",
+                tasks: "ts"
+            },
+
+            notify: {
+                files: "src/**",
+                tasks: "notify:watch"
+            }
+        },
+
+
+        // =============
+        // grunt-wiredep
+        // =============
+        wiredep: {
+            src: {
+                src: ["build/index.html"],
+                directory: "build/bower_components",
+                dependencies: true
+            }
         }
     });
 
-    grunt.registerTask('default', [
-        'clean',
-        'tpm-install',
-        'typescript'
+    // ==============
+    // Register Tasks
+    // ==============
+    grunt.registerTask("default", [
+        "clean",
+        "copy",
+        "wiredep",
+        "less:src",
+        "ts",
+        "notify:watch",
+        "watch"
     ]);
-}
+};
---
+++ src/dependencyGraph/dependencyGraphController.ts
@@ -0,0 +1,112 @@
+module CAFamilyTree.DependencyGraph {
+
+    export interface IDependencyGraphController {
+        graph: Dagre.Graph;
+    }
+
+    export interface IDependencyGraphScope extends ng.IScope {
+        dependencyGraphController: IDependencyGraphController;
+    }
+
+    export interface IRecitationInfo {
+        cas: string[];
+        students: string[];
+    }
+
+    export interface ISemesterRecitationInfo {
+        [semester: string]: { [recitation: string]: IRecitationInfo };
+    }
+
+    export class DependencyGraphController implements IDependencyGraphController {
+        public graph: Dagre.Graph;
+
+        constructor($http: ng.IHttpService, $scope: IDependencyGraphScope) {
+            $scope.dependencyGraphController = this;
+            $http.get<string>("assets/CAFamilyData.csv").then((response) => {
+                this.graph = this.constructGraph(response.data);
+            });
+        }
+
+        private constructGraph(csvData: string) {
+            // create CA -> semester taken 100/110/112 map
+            var semesterRecitationInfo: ISemesterRecitationInfo  = { };
+
+            var safeGet = (semester, recitation) => {
+                if (!semesterRecitationInfo[semester]) {
+                    semesterRecitationInfo[semester] = { };
+                }
+
+                if (!semesterRecitationInfo[semester][recitation]) {
+                    semesterRecitationInfo[semester][recitation] = {
+                        cas: [],
+                        students: [],
+                    };
+                }
+                return semesterRecitationInfo[semester][recitation];
+            };
+
+            var rows = csvData.split("\n").map((row) => row.split(","));
+            var header = rows[0].map((column) => (column.indexOf("-") === -1) ? column : column.split("-")[0]);
+            rows.slice(1).forEach((row) => {
+                var name = row[0];
+                var andrewId = row[1];
+                var year = row[2];
+                var recitation = row[3];
+                if (year && recitation) {
+                    safeGet(year, recitation).students.push(andrewId);
+                }
+                for (var semesterIndex = 4; semesterIndex < row.length; semesterIndex++) {
+                    if (row[semesterIndex] && row[semesterIndex].toUpperCase() === row[semesterIndex]) {
+                        var recitations = row[semesterIndex].split("");
+                        recitations.forEach((recitation) => safeGet(header[semesterIndex], recitation).cas.push(andrewId));
+                    }
+                }
+            });
+
+            // create graph
+            var graph = new dagreD3.graphlib.Graph()
+                .setGraph({})
+                .setDefaultEdgeLabel(() => { return { }; });
+            (<any>graph).graph().nodesep = 200;
+            (<any>graph).graph().ranksep = 150;
+
+            var seenNodes = { };
+            var addNode = (node) => {
+                if (!seenNodes[node]) {
+                    seenNodes[node] = true;
+                    graph.setNode(node, { label: node, class: node });
+                }
+            };
+            Object.keys(semesterRecitationInfo).forEach((semester) => {
+                Object.keys(semesterRecitationInfo[semester]).forEach((recitation) => {
+                    var recitations;
+                    if (recitation.length == 1) {
+                        recitations = [recitation];
+                    } else if (recitation.length == 2) {
+
+                    }
+                    var recitationInfo = semesterRecitationInfo[semester][recitation];
+                    if (recitationInfo.students.length > 0) {
+                        recitationInfo.students.forEach(addNode);
+                        recitationInfo.cas.forEach(addNode);
+                    }
+
+                    recitationInfo.cas.forEach((parent) => {
+                        recitationInfo.students.forEach((child) => {
+                            (<any>graph).setEdge(parent, child, {
+                                lineInterpolate: "basis"
+                            });
+                        });
+                    });
+                });
+            });
+
+            console.log(rows);
+            console.log(JSON.stringify(semesterRecitationInfo));
+
+            return graph;
+        }
+    }
+
+    caFamilyTree.controller("dependencyGraphController", DependencyGraphController);
+}
