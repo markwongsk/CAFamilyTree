c4a6fd929d2357ad7723ad08ce4c02b1b999458f
Mark Wong Siang Kai <mwong@palantir.com>
2015-04-02 03:55:44 -0700

    Almost done?

--- src/dependencyGraph/dependencyGraphController.ts
+++ src/dependencyGraph/dependencyGraphController.ts
@@ -4,8 +4,17 @@ module CAFamilyTree.DependencyGraph {
         graph: Dagre.Graph;
     }
 
-    export interface IDependencyGraphScope {
+    export interface IDependencyGraphScope extends ng.IScope {
         dependencyGraphController: IDependencyGraphController;
+    }
+
+    export interface IRecitationInfo {
+        cas: string[];
+        students: string[];
+    }
+
+    export interface ISemesterRecitationInfo {
+        [semester: string]: { [recitation: string]: IRecitationInfo };
     }
 
     export class DependencyGraphController implements IDependencyGraphController {
@@ -14,14 +23,77 @@ module CAFamilyTree.DependencyGraph {
         constructor($http: ng.IHttpService, $scope: IDependencyGraphScope) {
             $scope.dependencyGraphController = this;
             $http.get<string>("assets/CAFamilyData.csv").then((response) => {
-                this.graph = constructGraph(response.data);
+                this.graph = this.constructGraph(response.data);
             });
         }
 
         private constructGraph(csvData: string) {
+            // create CA -> semester taken 100/110/112 map
+            var semesterRecitationInfo: ISemesterRecitationInfo  = { };
+
+            var safeGet = (semester, recitation) => {
+                if (!semesterRecitationInfo[semester]) {
+                    semesterRecitationInfo[semester] = { };
+                }
+
+                if (!semesterRecitationInfo[semester][recitation]) {
+                    semesterRecitationInfo[semester][recitation] = {
+                        cas: [],
+                        students: [],
+                    };
+                }
+                return semesterRecitationInfo[semester][recitation];
+            };
+
+            var rows = csvData.split("\n").map((row) => row.split(","));
+            var header = rows[0].map((column) => (column.indexOf("-") === -1) ? column : column.split("-")[0]);
+            rows.slice(1).forEach((row) => {
+                var name = row[0];
+                var andrewId = row[1];
+                var year = row[2];
+                var recitation = row[3];
+                if (year && recitation) {
+                    safeGet(year, recitation).students.push(andrewId);
+                }
+                for (var semesterIndex = 4; semesterIndex < row.length; semesterIndex++) {
+                    if (row[semesterIndex]) {
+                        safeGet(header[semesterIndex], row[semesterIndex]).cas.push(andrewId);
+                    }
+                }
+            });
+
+            // create graph
             var graph = new dagreD3.graphlib.Graph()
                 .setGraph({})
-                .setDefaultEdgeLabel(() => { return {}; });
+                .setDefaultEdgeLabel(() => { return { }; });
+
+            var seenNodes = { };
+            var addNode = (node) => {
+                if (!seenNodes[node]) {
+                    seenNodes[node] = true;
+                    graph.setNode(node, { label: node });
+                }
+            };
+            Object.keys(semesterRecitationInfo).forEach((semester) => {
+                Object.keys(semesterRecitationInfo[semester]).forEach((recitation) => {
+                    var recitationInfo = semesterRecitationInfo[semester][recitation];
+                    if (recitationInfo.students.length > 0) {
+                        recitationInfo.students.forEach(addNode);
+                        recitationInfo.cas.forEach(addNode);
+                    }
+
+                    recitationInfo.cas.forEach((parent) => {
+                        recitationInfo.students.forEach((child) => {
+                            graph.setEdge(parent, child);
+                        });
+                    });
+                });
+            });
+
+            console.log(rows);
+            console.log(JSON.stringify(semesterRecitationInfo));
+
+            return graph;
         }
     }
 
--- src/dependencyGraph/dependencyGraphDirective.ts
+++ src/dependencyGraph/dependencyGraphDirective.ts
@@ -5,6 +5,45 @@ module CAFamilyTree.DependencyGraph {
         public replace = true;
         public restrict = "E";
         public templateUrl = "src/dependencyGraph/dependencyGraphTemplate.html";
+
+        public link = ($scope: IDependencyGraphScope, element: JQuery) => {
+            var watchOnce = $scope.$watch(() => {
+                return $scope.dependencyGraphController.graph;
+            }, (graph: Dagre.Graph) => {
+                if (graph != null) {
+                    var render = new dagreD3.render();
+                    var svg = d3.select(element.get(0)).append("svg");
+                    svg.attr("width", "100%");
+                    svg.attr("height", "100%");
+                    var svgGroup = svg.append("g");
+                    render(d3.select("svg g"), graph);
+
+                    // compute scale in/out
+                    var svgJQuery = $(element).find("svg");
+                    var width = svgJQuery.width();
+                    var height = svgJQuery.height();
+                    var scale = 1.0;
+                    if (graph.graph().width > width || graph.graph().height > height) {
+                        var innerScale = 0.75;
+                        scale = Math.min(width / graph.graph().width * innerScale, height / graph.graph().height * innerScale);
+                    }
+
+                    // Center the graph
+                    var xCenterOffset = (width - graph.graph().width * scale) / 2;
+                    var yCenterOffset = (height - graph.graph().height * scale) / 2;
+                    svgGroup.attr("transform", "translate( " + xCenterOffset + ", " + yCenterOffset + ")" + "scale(" + scale + ")");
+
+                    // set up zoom
+                    var zoom = d3.behavior.zoom().on("zoom", function() {
+                        svgGroup.attr("transform", "translate(" + d3.event.translate + ")" +
+                            "scale(" + d3.event.scale + ")");
+                    });
+                    svg.call(zoom);
+                    zoom.translate([xCenterOffset, yCenterOffset]).scale(scale);
+                    watchOnce();
+                }
+            });
+        };
     }
 
     caFamilyTree.directive("dependencyGraph", () => new DependencyGraphDirective());
